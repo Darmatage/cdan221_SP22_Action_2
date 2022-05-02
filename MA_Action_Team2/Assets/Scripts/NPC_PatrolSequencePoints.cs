d527549cb3e62994e2545a752151d2d965fd75d5
JasonWiser <JasonWiserArt@gmail.com>
2022-05-02 13:56:44 -0400

    Initial Shark art and functionality implemented

--- MA_Action_Team2/Assets/Scripts/NPC_PatrolSequencePoints.cs
+++ MA_Action_Team2/Assets/Scripts/NPC_PatrolSequencePoints.cs
@@ -11,31 +11,59 @@ public class NPC_PatrolSequencePoints : MonoBehaviour {
        public Transform[] moveSpots;
        private int nextSpot;
        public int startSpot = 0;
+	   private int previousSpot;
        public bool moveForward = true;
 
-       void Start(){
-              waitTime = startWaitTime;
-              nextSpot = startSpot;
-       }
 
-       void Update(){
-              transform.position = Vector2.MoveTowards(transform.position, moveSpots[nextSpot].position, speed * Time.deltaTime);
+	private bool FaceRight = true;
+	
+	
 
-              if (Vector2.Distance(transform.position, moveSpots[nextSpot].position) < 0.2f){
-                     if (waitTime <= 0){
-                            if (moveForward == true){nextSpot += 1;}
-                            else if (moveForward == false){nextSpot -= 1;}
-                            waitTime = startWaitTime;
-                     } else {
-                            waitTime -= Time.deltaTime;
-                     }
-              }
+	void Start(){
+		waitTime = startWaitTime;
+		nextSpot = startSpot;
+	}
 
-              if (nextSpot == 0) {
-                     moveForward = true;
-              }
-              else if (nextSpot == (moveSpots.Length -1)) {
-                     moveForward = false;
-              }
-       }
+	void Update(){
+		transform.position = Vector2.MoveTowards(transform.position, moveSpots[nextSpot].position, speed * Time.deltaTime);
+
+		if (Vector2.Distance(transform.position, moveSpots[nextSpot].position) < 0.2f){
+			if (waitTime <= 0){
+				if (moveForward == true){nextSpot += 1;}
+				else if (moveForward == false){nextSpot -= 1;}
+				waitTime = startWaitTime;
+			} else {
+				waitTime -= Time.deltaTime;
+			}
+		}
+
+		if (nextSpot == 0) {
+			moveForward = true;
+		}
+		else if (nextSpot == (moveSpots.Length -1)) {
+			moveForward = false;
+		}
+			 
+		//turn the enemy, WIP
+		previousSpot = nextSpot -1;
+		if (previousSpot < 0){previousSpot = moveSpots.Length -1;}
+		else if (previousSpot > moveSpots.Length -1){previousSpot = 0;}
+		
+		if (moveSpots[nextSpot].position.x < moveSpots[previousSpot].position.x){
+			enemyTurn();
+		}
+    }
+	
+	
+	private void enemyTurn(){
+		// NOTE: Switch player facing label
+		FaceRight = !FaceRight;
+
+		// NOTE: Multiply player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+	
+	
 }
