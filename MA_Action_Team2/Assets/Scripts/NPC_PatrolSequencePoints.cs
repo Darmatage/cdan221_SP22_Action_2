e6c2b661de771f661754e6db088ad8b18b975fb5
JasonWiser <JasonWiserArt@gmail.com>
2022-05-02 22:45:46 -0400

    Shark sequence player flipping!

--- MA_Action_Team2/Assets/Scripts/NPC_PatrolSequencePoints.cs
+++ MA_Action_Team2/Assets/Scripts/NPC_PatrolSequencePoints.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class NPC_PatrolSequencePoints : MonoBehaviour {
 
-       public float speed = 10f;
-       private float waitTime;
-       public float startWaitTime = 2f;
+	public float speed = 10f;
+	private float waitTime;
+	public float startWaitTime = 2f;
 
-       public Transform[] moveSpots;
-       private int nextSpot;
-       public int startSpot = 0;
-	   private int previousSpot;
-       public bool moveForward = true;
-
-
-	private bool FaceRight = true;
+	public Transform[] moveSpots;
+	public int startSpot = 0;
+	public bool moveForward = true;
+	
+	// Turning
+	public int nextSpot;
+	public int previousSpot;  
+	public bool faceRight = true;
 	
 	
-
 	void Start(){
 		waitTime = startWaitTime;
 		nextSpot = startSpot;
@@ -29,35 +28,43 @@ public class NPC_PatrolSequencePoints : MonoBehaviour {
 
 		if (Vector2.Distance(transform.position, moveSpots[nextSpot].position) < 0.2f){
 			if (waitTime <= 0){
-				if (moveForward == true){nextSpot += 1;}
-				else if (moveForward == false){nextSpot -= 1;}
+				if (moveForward == true){ previousSpot = nextSpot; nextSpot += 1;}
+				else if (moveForward == false){previousSpot = nextSpot; nextSpot -= 1;}
 				waitTime = startWaitTime;
 			} else {
 				waitTime -= Time.deltaTime;
 			}
 		}
 
-		if (nextSpot == 0) {
-			moveForward = true;
-		}
-		else if (nextSpot == (moveSpots.Length -1)) {
-			moveForward = false;
-		}
+		//switch moveemnt direction
+		if (nextSpot == 0) {moveForward = true;}
+		else if (nextSpot == (moveSpots.Length -1)) {moveForward = false;}
 			 
-		//turn the enemy, WIP
-		previousSpot = nextSpot -1;
+		//turning the enemy, WIP
 		if (previousSpot < 0){previousSpot = moveSpots.Length -1;}
 		else if (previousSpot > moveSpots.Length -1){previousSpot = 0;}
 		
-		if (moveSpots[nextSpot].position.x < moveSpots[previousSpot].position.x){
-			enemyTurn();
-		}
+		if ((previousSpot == 0)&&(!faceRight)){enemyTurn();}
+		else if ((previousSpot == (moveSpots.Length -1))&&(faceRight)) {enemyTurn();}
+		
+		
+		
+		// if (((MovingRight())&&(faceRight))||((MovingRight()==false)&&(!faceRight))){
+			// enemyTurn();
+		// }
     }
 	
 	
+	// public bool MovingRight(){
+		// if (moveSpots[nextSpot].position.x < moveSpots[previousSpot].position.x){
+			// return true; 
+		// }
+		// return false;
+	// }
+	
 	private void enemyTurn(){
-		// NOTE: Switch player facing label
-		FaceRight = !FaceRight;
+		// NOTE: Switch player facing label (avoids constant turning)
+		faceRight = !faceRight;
 
 		// NOTE: Multiply player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
