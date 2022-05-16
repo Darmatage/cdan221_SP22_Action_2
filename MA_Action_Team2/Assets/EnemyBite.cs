95ec07adf5d2b2c0cc5665ac6af46d7593fa48ac
JasonWiser <JasonWiserArt@gmail.com>
2022-05-15 22:19:28 -0400

    bite for patrolling enemies works!

--- MA_Action_Team2/Assets/EnemyBite.cs
+++ MA_Action_Team2/Assets/EnemyBite.cs
@@ -6,32 +6,63 @@ public class EnemyBite : MonoBehaviour{
 
 	private GameHandler gameHandler;
 	private Animator anim;
+	
+	private Transform player; 
 	public Transform AttackPoint;
-	public float attackRange = 3;
-	public int damage = 5;
+	public float attackRange = 5f;
+	public float damageRange = 7f;
 	public LayerMask playerLayers;
+
+	public int damage = 10;
+	float distanceToMouth;
+	public float timeToNextAttack = 2f;
+	public bool canAttack = true;
 
     // Start is called before the first frame update
     void Start(){
         gameHandler = GameObject.FindWithTag("GameHandler").GetComponentInChildren<GameHandler>();
 		anim = GetComponentInChildren<Animator>();
+		player = GameObject.FindWithTag("Player").transform;
+		
     }
+
+	void Update(){
+		distanceToMouth = Vector3.Distance(player.position, AttackPoint.position);
+		
+		if ((distanceToMouth < attackRange) && (canAttack)){
+			Attack();
+			StartCoroutine(AttackDelay());
+		}
+	}
 
 	
 	void Attack(){
-            anim.SetTrigger ("Attack");
-            Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(AttackPoint.position, attackRange, playerLayers);
+		Debug.Log("I am biting the playing! He he!");
+		anim.SetTrigger ("Attack");
+		if (distanceToMouth < damageRange){
+			gameHandler.playerGetHit(damage);
+		}
+		
+		// Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(AttackPoint.position, damageRange, playerLayers);
            
-            foreach(Collider2D player in hitPlayer){
-                  Debug.Log("We hit " + player.name);
-                  gameHandler.playerGetHit(damage);
-            }
+		// foreach(Collider2D player in hitPlayer){
+			// Debug.Log("We hit " + player.name);
+			// gameHandler.playerGetHit(damage);
+		// }
 	}
 	
 	//NOTE: to help see the attack sphere in editor:
       void OnDrawGizmos(){
-           if (AttackPoint == null) {return;}
             Gizmos.DrawWireSphere(AttackPoint.position, attackRange);
+			Gizmos.DrawWireSphere(AttackPoint.position, damageRange);
       }
+	  
+	  
+	IEnumerator AttackDelay(){
+		canAttack = false;
+		yield return new WaitForSeconds(timeToNextAttack);
+		canAttack = true;
+	}
+	  
 	
 }
